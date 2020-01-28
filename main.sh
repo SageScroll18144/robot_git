7d8414a7f1c6ff041786fb44ab2108310a94d9e6
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-28 16:28:07 -0300

    Não atribui o diretorio a variavel"

--- main.sh
+++ main.sh
@@ -4,9 +4,12 @@
 
 output=$(python3 Documentos/gitt/robot_git/path.py)
 
-cp Documentos/gitt/robot_git/robot_git.py /output
+echo output
+
+cp Documentos/gitt/robot_git/robot_git.py rbt_git.py
+
+mv rbt_git.py output/rbt_git.py
 
 cd output
 
-
-python3 Documentos/gitt/robot_git/robot_git.py
+python3 rbt_git.py
--- path.py
+++ path.py
@@ -1,8 +1,8 @@
 import os
 
 def path(file):
-    return os.path.abspath(file)[11::]
+    return os.path.abspath(file)[12::]
 
-in = input("What's the name of the repository? "
+inp = input("What's the name of the repository? ")
 
-print(path(in))
+print(path(inp))
