45ce78114634fffac253a4c282ea179fa9d81716
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-28 21:19:35 -0300

    update...

--- robot_git.py
+++ robot_git.py
@@ -10,4 +10,4 @@ if(i != "std"):
 
 cp = cmd.run("git add .", check=True, shell=True)
 cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
-cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+cp = cmd.run("git push", check=True, shell=True)
